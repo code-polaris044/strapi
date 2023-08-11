9ddddbad5c85ca07ef9de38595831c364bfbcabc
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-08-11 09:50:59 +0900

    first commit

---
+++ nuxt/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="nuxt__title">テストタイトル</h1>
+  <div></div>
+</template>
+<style>
+body {
+  height: 200px;
+  width: 90%;
+  background-image: linear-gradient(
+    90deg,
+    rgba(40, 0, 255, 1),
+    rgba(255, 160, 71, 1)
+  );
+}
+
+.nuxt__title {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 400;
+  text-align: center;
+}
+</style>
